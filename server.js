ed18c059493138c3fdc4ad2d23ab3a7189a1ae36
KunalSKurve <kunal.kurve19@vit.edu>
2024-08-12 23:33:59 +0530

    server updated

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-// server.js
 const express = require('express');
 const connectDB = require('./config/db');
 const dotenv = require('dotenv');
+const path = require('path');
 
 dotenv.config();
 
@@ -17,14 +17,14 @@ app.use(express.json());
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/scrape', require('./routes/scraper'));
 
-const PORT = process.env.PORT || 5001;
-
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-
-const path = require('path');
+// Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
 
+// Serve the React app
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
